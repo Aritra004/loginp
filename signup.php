9dd1fe357afd3bbee45943c19e89e0e3f769363e
Aritra Debnath <119656726+Aritra004@users.noreply.github.com>
2023-03-29 20:55:08 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,65 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $err=false;
+    include 'partials/_dbconnect.php';
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+    $cpassword=$_POST["cpassword"];
+    $exist=false;
+    if(($password==$cpassword) && ($exist==false)){
+        $sql="INSERT INTO `users` (`username`, `password`, `date`) VALUES ('$username', '$password', current_timestamp())";
+        $result=mysqli_query($conn,$sql);
+        if($result){
+            $err=true;
+        }
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Signup</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php require 'partials/_nav.php' ?>
+    
+    
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> Your account is now created and you can login.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+    
+    
+    <div class="container">
+        <h1 class="text-center">Signup to our Website</h1>
+        <form action="/loginsys/signup.php" method="post">
+  <div class="mb-3">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="mb-3">
+    <label for="cpassword" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="cpassword" name="cpassword">
+    <small id="emailHelp" class="form-text text-muted">Make sure to type the same password</small>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Signup</button>
+</form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+  </body>
+</html>
